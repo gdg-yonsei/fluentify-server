4cfaca92d001272f18639f09808fdba7a4278c90
Jason Yoo <jasonyoo0515@gmail.com>
2024-02-20 22:30:27 +0900

    feat: add speaking rate to audio condig

--- ai/talking-video-gen.py
+++ ai/talking-video-gen.py
@@ -51,7 +51,7 @@ class TalkingGenerator:
 
         # Select the type of audio file you want returned
         audio_config = texttospeech.AudioConfig(
-            audio_encoding=texttospeech.AudioEncoding.MP3)
+            audio_encoding=texttospeech.AudioEncoding.MP3, speaking_rate=0.6)
 
         # Perform the text-to-speech request on the text input with the selected
         # voice parameters and audio file type
